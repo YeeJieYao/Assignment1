739d0e5b51062f981cfbdc67b57bcba0a2ca2e2d
YeeJieYao <1221402113@student.mmu.edu.my>
2023-09-04 21:21:38 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,17 @@
 from dash import Dash, html, dcc, Output, Input
 import plotly.express as px
+import pandas as pd
 
 app = Dash(__name__)
 server = app.server
 
 image_path = 'https://www.mmu.edu.my/wp-content/themes/mmu2018/assets/images/logo-mmu.png'
+data_path = 'https://raw.githubusercontent.com/YeeJieYao/Assignment1/main/population.csv'
 
-fig = px.line(x=["a","b","c","d","e"], y=[1,3,2,2,3], title="Sample figure")	
+df = pd.read_csv(data_path)
+df2= df.iloc[1:]
+
+fig = px.pie(df2, values='pop',names='state', title='Malaysia Population') .update_layout(xaxis_title="State", yaxis_title="Index")
 
 df = px.data.iris()
 fig2 = px.scatter(df, x="sepal_width", y="sepal_length", color="species", title="A Plotly Express Figure")
